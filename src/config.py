05924fa1fc7b116d87f211942c33ccb9957138ca
Ivan <ivan.pavlovich.business@gmail.com>
2023-08-23 13:49:32 +0300

    chore: add env variables for auth

--- src/config.py
+++ src/config.py
@@ -4,6 +4,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings):
     DATABASE_URL: str
 
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_EXPIRE_MINUTES: int
+
+    REFRESH_EXPIRE_DAYS: int
+
     model_config = SettingsConfigDict(env_file=".env")
 
 
